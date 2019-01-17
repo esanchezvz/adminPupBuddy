9c9426018db1eef1efa39f29e4467f19406febd2
Esteban Batman <41240399+esanchezvz@users.noreply.github.com>
2019-01-17 10:53:49 -0600

    Edit paseadores component and routing

--- src/app/components/paseadores-editar/paseadores-editar.component.ts
+++ src/app/components/paseadores-editar/paseadores-editar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paseadores-editar',
@@ -26,7 +27,7 @@ export class PaseadoresEditarComponent implements OnInit {
     return {...item, selected: false}
   })
 
-  constructor() {
+  constructor(private router: Router) {
     this.selectHandler = (elemID, index) => {
       this.items.forEach(item => {
         if (elemID === item.id) {
@@ -34,6 +35,7 @@ export class PaseadoresEditarComponent implements OnInit {
           console.log(item)
         }
       });
+    this.router.navigate([`paseadores/editar/${elemID}`]);
     }
   }
 
